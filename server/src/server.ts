a41a93f68a566378d48d43e45e3ae20ec1b21bb8
Chuks-x <paradoxlegendz@gmail.com>
2025-02-08 15:09:45 -0600

    Migrate new prisma schema and create prisma client

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import tripRoutes from './routes/tripRoutes.ts';
+import prisma from './config/prismaClient.ts'
 
 const port: number | string = process.env.PORT || 3000;
 
@@ -12,4 +13,10 @@ app.use('/trips', tripRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
+});
+
+// Handle Prisma Client shutdown gracefully
+process.on("SIGINT", async () => {
+  await prisma.$disconnect();
+  process.exit(0);
 });
