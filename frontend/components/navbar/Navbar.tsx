e17dd3f3b18482cc4013c6523017ec36b7679127
Raman Bhandari <ramanbhandarieoes@gmail.com>
2025-02-19 16:49:23 -0600

    Fix meno icon button color for light mode

--- frontend/components/navbar/Navbar.tsx
+++ frontend/components/navbar/Navbar.tsx
@@ -1,8 +1,17 @@
 "use client";
 
 import Link from "next/link";
-import { AppBar, Toolbar, Box, Typography, Menu, MenuItem, IconButton, ListItemIcon } from "@mui/material";
-import {LogoutTwoTone} from "@mui/icons-material";
+import {
+  AppBar,
+  Toolbar,
+  Box,
+  Typography,
+  Menu,
+  MenuItem,
+  IconButton,
+  ListItemIcon,
+} from "@mui/material";
+import { LogoutTwoTone } from "@mui/icons-material";
 import ThemeToggle from "../ThemeToggle";
 import { useEffect, useState } from "react";
 import { supabase } from "@/utils/supabase/client";
@@ -12,7 +21,7 @@ import { usePathname, useRouter } from "next/navigation";
 import AnimatedMenuIcon from "./hamAnimIcon";
 
 export default function Navbar() {
-  const [user, setUser ] = useState<User | null>(null);
+  const [user, setUser] = useState<User | null>(null);
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
   const router = useRouter();
@@ -23,25 +32,27 @@ export default function Navbar() {
 
   useEffect(() => {
     // Fetch user on initial mount and set the user state if authenticated
-    const fetchUser  = async () => {
-      const { data } = await supabase.auth.getUser ();
-      setUser (data.user);
+    const fetchUser = async () => {
+      const { data } = await supabase.auth.getUser();
+      setUser(data.user);
     };
 
-    fetchUser (); // Call once on mount
+    fetchUser(); // Call once on mount
 
     // Listen for auth state changes (login/logout)
-    const { data: authListener } = supabase.auth.onAuthStateChange((event, session) => {
-      if (session) {
-        setUser (session.user);
-      } else {
-        setUser (null);
-        // Redirect to login page after logout
-        if (!isLoginPage) {
-          router.push("/login");
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      (event, session) => {
+        if (session) {
+          setUser(session.user);
+        } else {
+          setUser(null);
+          // Redirect to login page after logout
+          if (!isLoginPage) {
+            router.push("/login");
+          }
         }
       }
-    });
+    );
 
     // Cleanup the listener on unmount
     return () => {
@@ -59,13 +70,16 @@ export default function Navbar() {
 
   const handleLogout = async () => {
     await logout();
-    setUser (null);
+    setUser(null);
   };
 
   return (
     <AppBar position="fixed" sx={{ bgcolor: "primary.main" }}>
       <Toolbar className="flex justify-between items-center px-4">
-        <Typography variant="h4" sx={{ color: "#ffffff", fontFamily: "var(--font-brand)" }}>
+        <Typography
+          variant="h4"
+          sx={{ color: "#ffffff", fontFamily: "var(--font-brand)" }}
+        >
           <Link href="/"> {!isLoginPage ? "Vamoose!" : " "}</Link>
         </Typography>
 
@@ -76,6 +90,7 @@ export default function Navbar() {
               <IconButton
                 aria-label="menu"
                 onClick={handleMenuOpen}
+                color="inherit"
               >
                 <AnimatedMenuIcon isOpen={open} onClick={handleMenuOpen} />
               </IconButton>
@@ -84,7 +99,7 @@ export default function Navbar() {
                 open={open}
                 onClose={handleMenuClose}
                 MenuListProps={{
-                  'aria-labelledby': 'basic-button',
+                  "aria-labelledby": "basic-button",
                 }}
                 sx={{
                   "& .MuiPaper-root": {
@@ -98,14 +113,16 @@ export default function Navbar() {
                     <Link href="/dashboard">Dashboard</Link>
                   </MenuItem>
                 )}
-                <MenuItem onClick={() => {
-                  handleMenuClose();
-                  handleLogout();
-                }}>
-                <ListItemIcon>
-                <LogoutTwoTone/>
-                </ListItemIcon>
-                Logout
+                <MenuItem
+                  onClick={() => {
+                    handleMenuClose();
+                    handleLogout();
+                  }}
+                >
+                  <ListItemIcon>
+                    <LogoutTwoTone />
+                  </ListItemIcon>
+                  Logout
                 </MenuItem>
               </Menu>
             </div>
