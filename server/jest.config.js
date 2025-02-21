61d56b02715ce5c63e5cbe4d17dd4dcaa969a482
Anmolpreet Khangura <109428188+AnmolKhangura@users.noreply.github.com>
2025-02-20 18:07:38 -0600

    fix jest config

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,4 +1,10 @@
-export const preset = 'ts-jest';
-export const testEnvironment = 'node';
-export const clearMocks = true;
-// export const setupFilesAfterEnv = ["<rootDir>/src/tests/jest.setup.ts"]; // Will enable for Integration testing
+// export const preset = 'ts-jest';
+// export const testEnvironment = 'node';
+// export const clearMocks = true;
+// // export const setupFilesAfterEnv = ["<rootDir>/src/tests/jest.setup.ts"]; // Will enable for Integration testing
+
+export default {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    clearMocks: true,
+};
