b2a0b9b565ed4ed05ffab5c9fb1db4d6c91a6834
Anmolpreet Singh <anek.timitos88@gmail.com>
2025-02-21 16:30:30 -0600

    fix default export issue for jest.config.js

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,4 +1,6 @@
-export const preset = 'ts-jest';
-export const testEnvironment = 'node';
-export const clearMocks = true;
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  clearMocks: true,
+};
 // export const setupFilesAfterEnv = ["<rootDir>/src/tests/jest.setup.ts"]; // Will enable for Integration testing
