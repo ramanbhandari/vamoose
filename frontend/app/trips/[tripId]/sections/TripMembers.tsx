8bbf5f65aadad443caa7e9fa9b1d63f266603208
Anmolpreet Khangura <109428188+AnmolKhangura@users.noreply.github.com>
2025-02-24 10:12:43 -0600

    add ui to invite members

--- frontend/app/trips/[tripId]/sections/TripMembers.tsx
+++ frontend/app/trips/[tripId]/sections/TripMembers.tsx
@@ -1,15 +1,72 @@
-import { Box, Typography, Divider } from "@mui/material";
+"use client";
+
+import InviteModal from "@/components/InviteModal";
+import { GroupAdd } from "@mui/icons-material";
+import { Box, Typography, Divider, Button } from "@mui/material";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function TripMembers() {
+
+  const [isInviteModalOpen, setInviteModalOpen] = useState(false);
+
+  const handleOpenInviteModal = () => {
+    setInviteModalOpen(true);
+  };
+
+  const handleCloseInviteModal = () => {
+    setInviteModalOpen(false);
+  };
+
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const invite = searchParams.get("invite");
+    if (invite === "true") {
+      handleOpenInviteModal();
+      router.replace(`${pathname}`);
+    }
+  }, [searchParams]);
+  
   return (
-    <Box>
-      <Typography variant="h5" fontWeight="bold">
-        Trip Members
-      </Typography>
-      <Divider sx={{ my: 2 }} />
-      <Typography variant="body1">
-        A list of all members included in this trip.
-      </Typography>
-    </Box>
+    <>
+      <Box>
+        <Box
+        sx={{
+          display: "flex",
+          alignItems: "center", 
+          justifyContent: "space-between", 
+          width: "100%", 
+          mb: 3, 
+        }}
+      >
+        {/* Header */}
+          <Typography variant="h5" fontWeight="bold">
+            Trip Members
+          </Typography>
+            <Button
+              variant="outlined"
+              color="secondary"
+              startIcon={<GroupAdd />}
+              sx={{
+                borderRadius: 3,
+                py: 1.5,
+                fontSize: "0.8rem",
+              }}
+              onClick={handleOpenInviteModal} 
+            >
+              Invite More Explorers
+            </Button>
+        </Box>
+        <Divider sx={{ my: 2 }} />
+        <Typography variant="body1">
+          A list of all members included in this trip.
+        </Typography>
+      </Box>
+      <InviteModal open={isInviteModalOpen} onClose={handleCloseInviteModal} />
+    </>
+
   );
 }
