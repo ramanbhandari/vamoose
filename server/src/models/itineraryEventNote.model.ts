83e3510a09b56ded5377c5a1dcf9157a8679d6ac
Anmolpreet Khangura <109428188+AnmolKhangura@users.noreply.github.com>
2025-03-12 00:30:35 -0500

    update model to support update event note

--- server/src/models/itineraryEventNote.model.ts
+++ server/src/models/itineraryEventNote.model.ts
@@ -1,5 +1,6 @@
 import prisma from '@/config/prismaClient.js';
 import { handlePrismaError } from '@/utils/errorHandlers.js';
+import { DateTime } from 'luxon';
 
 // Add a note to an event
 export const addNoteToItineraryEvent = async (
@@ -17,6 +18,35 @@ export const addNoteToItineraryEvent = async (
     });
   } catch (error) {
     console.error('Error adding event note:', error);
+    throw handlePrismaError(error);
+  }
+};
+
+// Get a note by id
+export const getItineraryEventNoteById = async (noteId: number) => {
+  try {
+    return await prisma.eventNote.findUnique({
+      where: { id: noteId },
+    });
+  } catch (error) {
+    console.error('Error getting event note:', error);
+    throw handlePrismaError(error);
+  }
+};
+
+// Update a note
+export const updateItineraryEventNote = async (
+  noteId: number,
+  userId: string,
+  content: string,
+) => {
+  try {
+    return await prisma.eventNote.update({
+      where: { id: noteId },
+      data: { content, updatedAt: DateTime.now().toUTC().toJSDate() },
+    });
+  } catch (error) {
+    console.error('Error updating event note:', error);
     throw handlePrismaError(error);
   }
 };
