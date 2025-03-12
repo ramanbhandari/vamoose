334035b440a2ca429c26c25c67634862cae34ed1
Anmolpreet Khangura <109428188+AnmolKhangura@users.noreply.github.com>
2025-03-12 09:30:00 -0500

    update routes to support delete functionality

--- server/src/routes/itineraryEventNote.routes.ts
+++ server/src/routes/itineraryEventNote.routes.ts
@@ -2,10 +2,14 @@ import express from 'express';
 import validationErrorHandler from '@/middleware/validationErrorHandler.js';
 import {
   validateAddEventNoteInput,
+  validateBatchDeleteEventNotesInput,
+  validateDeleteEventNoteInput,
   validateUpdateEventNoteInput,
 } from '@/middleware/itineraryEventNote.validators.js';
 import {
   addNoteToItineraryEventHandler,
+  batchDeleteItineraryEventNotesHandler,
+  deleteItineraryEventNoteHandler,
   updateItineraryEventNoteHandler,
 } from '@/controllers/itineraryEventNote.controller.js';
 
@@ -23,6 +27,20 @@ router.patch(
   validateUpdateEventNoteInput,
   validationErrorHandler,
   updateItineraryEventNoteHandler,
+);
+
+router.delete(
+  '/:noteId',
+  validateDeleteEventNoteInput,
+  validationErrorHandler,
+  deleteItineraryEventNoteHandler,
+);
+
+router.delete(
+  '/',
+  validateBatchDeleteEventNotesInput,
+  validationErrorHandler,
+  batchDeleteItineraryEventNotesHandler,
 );
 
 export default router;
