3cc54334ca14b2e68177f9fe6f1beedf968c6cff
Raman Bhandari <ramanbhandarieoes@gmail.com>
2025-03-12 20:02:24 -0500

    302 integrate notifications api with UI (#304)
    * Notification bell and list view in navbar UI with fake data
    
    * created user notifications store
    
    * Fetch notifications on navbar mount if valid user
    
    * Utilize user notifications store to update notf list
    
    * added realtime debug to supabase client
    
    * Added section navigation capabilities in trips/id route using hash
    
    * Navigate to correct trip and section on notification click
    
    * Updated UI and added Notif type specific icons map
    
    * 277 implement endpoint for updating itinerary events (#293)
    
    * add interface for update itineraryEvent
    
    * add validator for update itineraryEvent
    
    * add models for update and fetch itinerary event
    
    * create update itineraryEvent handler
    
    * add update itinerary event route
    
    * add unit test for update itinerary event
    
    * fix lint issue
    
    * add validator tests for validateUpdateItineraryEventInput
    
    * fix pr comments
    
    * Delete .vscode directory
    
    * remove .DS_Store
    
    * add start time and end time check to update itinerary event
    
    * update tests for the itinerary event controllers
    
    * fix date issue
    
    * handle pr comments
    
    ---------
    
    Co-authored-by: Anmolpreet Khangura <109428188+AnmolKhangura@users.noreply.github.com>

--- frontend/utils/supabase/client.ts
+++ frontend/utils/supabase/client.ts
@@ -3,7 +3,12 @@ import { createBrowserClient } from "@supabase/ssr";
 export const createClient = () =>
   createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      realtime: {
+        log_level: "debug",
+      },
+    }
   );
 
 export const supabase = createClient();
