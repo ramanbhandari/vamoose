9aaada53cf84a221770c841b94db4108366bb97c
Raman Bhandari <ramanbhandarieoes@gmail.com>
2025-03-17 19:19:03 -0500

    establish the props and structure for our calendar view

---
+++ frontend/app/trips/[tripId]/sections/Itinerary/CalendarView/CalendarView.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+import { CreateItineraryEvent, CreateNote, ItineraryEvent } from "../types";
+import { Member } from "@/types";
+
+interface CalendarViewProps {
+  selectedDate?: Date;
+  onAdd: (eventData: CreateItineraryEvent) => void;
+  onUpdate: (event: ItineraryEvent, payload: CreateItineraryEvent) => void;
+  onDelete: (eventId: number) => void;
+  onAddNote: (eventId: number, note: CreateNote) => void;
+  onUpdateNote: (eventId: number, noteId: number, note: CreateNote) => void;
+  onDeleteNote: (eventId: number, noteId: number) => void;
+  onEventSelect?: (event: ItineraryEvent) => void;
+  onAssignMembers: (eventId: number, userIds: string[]) => void;
+  onUnAssignMembers: (eventId: number, userIds: string[]) => void;
+  members: Member[];
+  tripStart: string;
+  tripEnd: string;
+  loading?: boolean;
+  error?: string | null;
+}
+
+const CalendarView: React.FC<CalendarViewProps> = ({
+  loading = false,
+  error = null,
+  members,
+  tripStart,
+  tripEnd,
+  onAdd,
+  onUpdate,
+  onDelete,
+  onAddNote,
+  onUpdateNote,
+  onDeleteNote,
+  onAssignMembers,
+  onUnAssignMembers,
+  onEventSelect = () => {},
+}) => {
+  return <></>;
+};
+
+export default CalendarView;
