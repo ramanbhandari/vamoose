9ccfe8c5f9c782e02cd96065be27b071e2d3e22c
Anmolpreet Singh <62654357+Qprah@users.noreply.github.com>
2025-03-21 11:00:54 -0500

    337 implement message api integration tests (#382)
    * setup dockerfile for mongodb
    
    * update mongo connection to check for test environment
    
    * update the test script to start the test mongo db
    
    * add mongo db in server-ci
    
    * add integration tests for messages
    
    * test ci fix
    
    * fix env name for mongo
    
    * add update reaction, remove reactions, unaothorized access, non-mmber access test
    
    * add get all messages test
    
    * fix lint error
    
    ---------
    
    Co-authored-by: Anmolpreet Khangura <109428188+AnmolKhangura@users.noreply.github.com>

--- server/src/db/mongo.ts
+++ server/src/db/mongo.ts
@@ -5,10 +5,16 @@ dotenv.config();
 
 const ATLAS_MONGO_URI = `mongodb+srv://${process.env.MONGO_ATLAS_USERNAME}:${process.env.MONGO_ATLAS_PASSWORD}@${process.env.MONGO_ATLAS_CLUSTER}.vzhwu.mongodb.net/?retryWrites=true&w=majority&appName=${process.env.MONGO_ATLAS_CLUSTER}`;
 
+// Use a different URI for testing
+const MONGO_TEST_URI = process.env.MONGO_TEST_URI || '';
+
+// Use the test URI if the NODE_ENV is test
+const URI = process.env.NODE_ENV === 'test' ? MONGO_TEST_URI : ATLAS_MONGO_URI;
+
 const connectMongoDB = async (): Promise<void> => {
   try {
-    await mongoose.connect(ATLAS_MONGO_URI);
-    console.log('MongoDB Atlas Connected');
+    await mongoose.connect(URI);
+    console.log('MongoDB Atlas Connected', URI);
   } catch (error) {
     console.error('MongoDB AtlasConnection Error:', error);
     process.exit(1);
